c8908be1238e6d8620c5dfeb721db3d358159bf9
giank <gsgiank@gmail.com>
2019-04-06 13:17:15 -0600

    primer commit

---
+++ Servicios.UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using Servicios.Repositories;
+
+namespace Servicios.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        ICustomerRepository Customer { get; }
+        IUserRepository User { get; }
+        ISupplierRepository Supplier { get; }
+        IOrderRepository Order { get; }
+    }
+}
